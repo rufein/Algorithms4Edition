8c3021ea5667d989356813e61ceacc6f2de04ede
rufein <koldo-glez@gmail.com>
2013-12-02 19:29:49 +0100

    Binary Search 2 arguments

--- chapter 1/1.1/exercise_1_1_23.java
+++ chapter 1/1.1/exercise_1_1_23.java
@@ -2,12 +2,20 @@
  *  Exercise 1.1.23
  */
 
+/*
+ *  Execution:
+ * 
+ *  % java BinarySearch tinyW.txt + < tinyT.txt
+ * 
+ *  % java BinarySearch tinyW.txt - < tinyT.txt
+*/
+
 
 import java.util.Arrays;
 
-public class exercise_1_1_23{
-    
-     // precondition: array a[] is sorted
+public class exercise_1_1_23 {
+
+    // precondition: array a[] is sorted
     public static int rank(int key, int[] a) {
         int lo = 0;
         int hi = a.length - 1;
@@ -23,16 +31,19 @@ public class exercise_1_1_23{
 
     public static void main(String[] args) {
         In in = new In(args[0]);
-        int[] whitelist = in.readInts();
+        int[] whitelist = in.readAllInts();
 
         Arrays.sort(whitelist);
 
         // read key; print if not in whitelist
         while (!StdIn.isEmpty()) {
             int key = StdIn.readInt();
-            if (rank(key, whitelist) == -1)
+            if ( rank(key, whitelist) == -1 && args[1].equals("+") ){
                 StdOut.println(key);
+            }
+            else if( rank(key, whitelist) > 0 && args[1].equals("-") ){
+                StdOut.println(key);
+            }
         }
     }
-
 }
